b96e29aa6be21b73e406bb39edc42c67e64e8b26
tetric0 <victor.munoz@upc.edu>
2021-10-29 11:27:54 +0200

    Ejercicios de video 07. Control de flujo con directivas

---
+++ app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            name: 'Bitcoin',
+            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+            changePercent: 0
+        }
+    }
+})
