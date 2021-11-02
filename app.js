ec4e90ba78db4b6587b80ef5d72d25a5f5739ef6
tetric0 <victor.munoz@upc.edu>
2021-11-02 09:14:21 +0100

    Ejercicio '10. Clases en tiempo real' completado

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ new Vue({
         return {
             name: 'Bitcoin',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 0
+            changePercent: -10,
+            price: 8400,
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 7900 },
+                { day: 'Miércoles', value: 8200 },
+                { day: 'Jueves', value: 9000 },
+                { day: 'Viernes', value: 9400 },
+                { day: 'Sábado', value: 10000 },
+                { day: 'Domingo', value: 10200 },
+            ],
+            showPrices: false,
+        }
+    },
+
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
         }
     }
 })
