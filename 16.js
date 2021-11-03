a129f607b51cf38740bf424bd0bd6aa28f159e52
tetric0 <victor.munoz@upc.edu>
2021-11-03 11:14:25 +0100

    Ejercicios '15. Sistema de componentes' y '16. Crear Componentes Custom' completados

---
+++ 16.js
@@ -0,0 +1,30 @@
+Vue.component('counter', {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+
+    methods: {
+        increment() {
+            this.counter += 1
+        }
+    },
+
+    template: `
+    <div>
+       <button v-on:click="increment">Click me!</button>
+       <span>{{ counter }}</span>
+    </div>
+    `
+})
+
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            titulo: '15. Crear Componentes Custom'
+        }
+    }
+})
