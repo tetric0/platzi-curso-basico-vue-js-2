3287bcc3870e6e6d0552d97825a119d8478df6fe
tetric0 <victor.munoz@upc.edu>
2021-11-03 13:26:48 +0100

    18. Comunicación entre componentes. Eventos

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ Vue.component('CoinDetail', {
     methods: {
         toggleShowPrices() {
             this.showPrices = !this.showPrices
+
+            this.$emit('change-color', this.showPrices ? 'FF96C8' : '3D3D3D')
         }
     },
 
@@ -91,10 +93,12 @@ new Vue({
         }
     },
 
-    //    methods: {
-    //        toggleShowPrices() {
-    //            this.showPrices = !this.showPrices
-    //            this.color = this.color.split('').reverse().join('')
-    //        }
-    //    }
+    methods: {
+        updateColor(color) {
+            this.color = color || this.color
+                .split('')
+                .reverse()
+                .join('')
+        }
+    }
 })
