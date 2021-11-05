09d858312589fe4e80da727d6c1e1aaa4e6a56fc
tetric0 <victor.munoz@upc.edu>
2021-11-05 11:36:15 +0100

    Ejercicio '22.practica_modulo_3' completado.

---
+++ 22.practica_modulo_3.js
@@ -0,0 +1,44 @@
+// Fork del proyecto en CodePen: https://codepen.io/ianaya89/pen/LYYJzor
+Vue.component('modal', {
+    props: [
+        'titulo',
+    ],
+
+    template: `
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <h3>{{ titulo }}</h3>
+            <div>
+              <slot name="body"></slot>
+            </div>
+            <footer>
+              <button v-on:click="close">Cerrar</button>
+            </footer>
+          </div>
+        </div>
+      </div>`,
+
+    methods: {
+        close() {
+            this.$emit('close')
+        }
+    }
+})
+
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            showModal: false,
+            title: 'Atención',
+        }
+    },
+
+    methods: {
+        toggleModal() {
+            this.showModal = !this.showModal
+        }
+    }
+})
